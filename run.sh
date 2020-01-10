1ff0db5bf8b9bacafc13f4d6fa4606285b859329
Björn Evers <2787957+Bjorn48@users.noreply.github.com>
2020-01-10 14:41:27 +0100

    Add bash run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+iterations=$1
+classes_file=$2
+process_limit=$3
+search_budget=$4
+exec_count_file=$5
+
+docker run -it --rm --mount type=bind,source="$(pwd)/analysis-result",target=/app/analysis-result \
+--mount type=bind,source="$(pwd)/consoleLog",target=/app/consoleLog \
+--mount type=bind,source="$(pwd)/evosuite-report",target=/app/evosuite-report \
+--mount type=bind,source="$(pwd)/generated_tests",target=/app/generated_tests \
+--mount type=bind,source="$(pwd)/logs",target=/app/logs \
+--mount type=bind,source="$(pwd)/results",target=/app/results \
+--mount type=bind,source="$(pwd)/exec_counts",target=/app/exec_counts \
+beversnl/es-model-seeding:0.1-SNAPSHOT $iterations $classes_file $process_limit $search_budget $exec_count_file
