8492c87613f1aa6968a260aecb9f6657e935ec72
Björn Evers <2787957+Bjorn48@users.noreply.github.com>
2020-01-10 13:11:05 +0100

    Add Windows run script

---
+++ run.ps1
@@ -0,0 +1,14 @@
+$iterations=$args[0]
+$classes_file=$args[1]
+$process_limit=$args[2]
+$search_budget=$args[3]
+$exec_count_file=$args[4]
+
+docker run -it --rm --mount type=bind,source="${pwd}\analysis-result",target=/app/analysis-result \
+--mount type=bind,source="${pwd}\consoleLog",target=/app/consoleLog \
+--mount type=bind,source="${pwd}\evosuite-report",target=/app/evosuite-report \
+--mount type=bind,source="${pwd}\generated_tests",target=/app/generated_tests \
+--mount type=bind,source="${pwd}\logs",target=/app/logs \
+--mount type=bind,source="${pwd}\results",target=/app/results \
+--mount type=bind,source="${pwd}\exec_counts",target=/app/exec_counts \
+beversnl/es-model-seeding:0.1-SNAPSHOT $iterations $classes_file $process_limit $search_budget $exec_count_file
